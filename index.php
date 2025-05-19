056c983c6e20f413a01dd237a7781711bebec672
Ludovic <dormoy.dev@gmail.com>
2025-05-19 15:27:54 +0200

    final push
    can remove todo and validate or cancel if the task is done

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 const ERROR_REQUIRED = 'Veuillez renseigner une todo';
 const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
-
 $filename = __DIR__ . "/data/todos.json";
 $error = '';
 $todos = [];
+$todo = '';
 
 if (file_exists($filename)) {
     $data = file_get_contents($filename);
@@ -25,12 +25,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $todos = [...$todos, [
             'name' => $todo,
             'done' => false,
-            "id" => time()
+            'id' => time()
         ]];
         file_put_contents($filename, json_encode($todos));
+        header('Location: /');
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -53,13 +55,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="todo-container">
             <h1>Ma Todo</h1>
             <form class="todo-form" action="/" method="post">
-                <input name="todo" type="text">
+                <input value="<?= $todo ?>" name="todo" type="text">
                 <button class="btn btn-primary">Ajouter</button>
             </form>
             <?php if ($error) : ?>
                 <p class="text-danger"><?= $error ?></p>
             <?php endif; ?>
-            <div class="todo-list"></div>
+            <ul class="todo-list">
+                <?php foreach ($todos as $t) : ?>
+                    <li class="todo-item <?= $t['done'] ? 'low-opacity' : '' ?>">
+                        <span class="todo-name"><?= $t['name'] ?></span>
+                        <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                            <button class="btn btn-primary btn-small"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+                        </a>
+                        <a href="/remove-todo.php?id=<?= $t['id'] ?>">
+                            <button class="btn btn-danger btn-small">Supprimer</button>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
 
     </div>
